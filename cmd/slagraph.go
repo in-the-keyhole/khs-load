408bd99e6feb3e1e1d71e2e03070c33399b1e10d
mauget <mauget@gmail.com>
2022-01-17 20:13:41 -0500

    SLA short and long description for Cobra

--- cmd/slagraph.go
+++ cmd/slagraph.go
@@ -1,5 +1,18 @@
+// Package cmd
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright 2022 Keyhole Software (http://keyholesoftware.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 */
 package cmd
@@ -17,13 +30,11 @@ import (
 // graphCmd represents the graph command
 var graphCmd = &cobra.Command{
 	Use:   "sla",
-	Short: "Line graph of ",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Renders a PNG file from a CSV file of response times vs time offsets",
+	Long: `Produces a PNG file rendition of a ling graph connecting points defined in a CSV file.
+Each point contains a response time versus a time offset. A rendered title states
+the total number of requests with the total that exceeded the SLA limit.`,
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
 		file, _ := cmd.Flags().GetString("file")
@@ -83,7 +94,7 @@ func slaSamplings() []Call {
 	//	sampleSize := 20
 	for i := range calls {
 
-		if int(calls[i].Time) > Sla() {
+		if calls[i].Time > Sla() {
 			results = append(results, calls[i])
 		}
 
